f6d9a4addfc092f4447360a2af1134525e53bda4
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-23 16:58:01 -0700

    wip: adds 4th project and styles tabs

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,35 +11,65 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Blackjack Game",
+      liveUrl: "https://carlosm22700.github.io/blackjack-project-one/",
+      codeUrl: "https://github.com/carlosm22700/blackjack-project-one",
+      details: [
+        {
+          description: "Screenshot 1 description",
+          imgUrl: projImg1,
+        },
+        {
+          description: "Screenshot 2 description",
+          imgUrl: projImg2,
+        },
+      ],
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "AniDex",
+      liveUrl: "https://anidex-app.herokuapp.com/",
+      codeUrl: "https://github.com/carlosm22700/Anime-Progress-Tracker",
+      details: [
+        {
+          description: "Screenshot 1 description",
+          imgUrl: projImg3,
+        },
+        {
+          description: "Screenshot 2 description",
+          imgUrl: projImg1,
+        },
+      ],
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: "NEED",
+      liveUrl: "https://need-829.herokuapp.com/",
+      codeUrl: "https://github.com/carlosm22700/NEED-ecommerce",
+      details: [
+        {
+          description: 'Screenshot 1 des',
+          imgUrl: projImg1,
+        },
+        {
+          description: 'Screenshot 2 des',
+          imgUrl: projImg2
+        },
+      ]
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
+      title: "FitTrax",
+      liveUrl: "https://fittrax.herokuapp.com/accounts/login/?next=/",
+      codeUrl: "https://github.com/carlosm22700/Fitness-Tracker",
+      details: [
+        {
+          description: 'Screenshot 1 des',
+          imgUrl: projImg1,
+        },
+        {
+          description: 'Screenshot 2 des',
+          imgUrl: projImg2
+        },
+      ]
+    }
   ];
 
   return (
@@ -49,47 +79,42 @@ export const Projects = () => {
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                title={project.title}
-                                description={project.description}
-                                imgUrl={project.imgUrl}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                  <h2>Projects</h2>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="0">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                      {
+                        projects.map((project, index) => (
+                          <Nav.Item key={index}>
+                            <Nav.Link eventKey={index}>{project.title}</Nav.Link>
+                          </Nav.Item>
+                        ))
+                      }
+                    </Nav>
+                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                      {
+                        projects.map((project, index) => (
+                          <Tab.Pane eventKey={index} key={index}>
+                            <Row>
+                              {
+                                project.details.map((detail, idx) => (
+                                  <ProjectCard
+                                    key={idx}
+                                    title={project.title}
+                                    description={detail.description}
+                                    imgUrl={detail.imgUrl}
+                                  />
+                                ))
+                              }
+                            </Row>
+                          </Tab.Pane>
+                        ))
+                      }
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              }
             </TrackVisibility>
           </Col>
         </Row>
