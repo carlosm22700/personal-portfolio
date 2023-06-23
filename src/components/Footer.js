32008c4954b6d0c251d2b5047d832e367b11b0cb
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-22 23:47:17 -0700

    cleans up code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,9 +17,8 @@ export const Footer = () => {
                     </Col>
                     <Col sm={6} className='text-center text-sm-end'>
                         <div className="social-icon">
-                            <a href=""><img src={navIcon1} /></a>
-                            <a href=""><img src={navIcon2} /></a>
-                            <a href=""><img src={navIcon3} /></a>
+                            <a href='https://www.linkedin.com/in/carlossmartinez/' target='_blank'><img src={navIcon1} alt=''/></a>
+                            <a href='https://github.com/carlosm22700' target='_blank'><img src={navIcon2} alt=''/></a>
                         </div>
                         <p>CopyRight 2022. All Rights Reserved - Carlos Martinez</p>
                     </Col>
