b7112810bb2f47a4fc39f36d5589b89fd90aae71
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-22 22:31:26 -0700

    adds footer and newsletter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Footer />
     </div>
   );
 }
