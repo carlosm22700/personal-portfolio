b38bf05bf09b7b641400879f1a25965019ea8b89
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-23 19:01:41 -0700

    updates logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import { Row, Col, Container, } from "react-bootstrap"
 import { MailchimpForm } from "./MailchimpForm"
-import logo from '../assets/img/logo.svg';
+import logo from '../assets/img/logo2.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '../assets/img/logo.svg';
+import logo from '../assets/img/logo2.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
