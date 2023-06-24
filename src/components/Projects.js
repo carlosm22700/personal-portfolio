4f0e526e973ccf3a6030fe951e6c115995f8aea9
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-23 18:11:35 -0700

    adds images to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,30 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import proj1Img1 from "../assets/img/Blackjack/proj1Img1.png";
+import proj1Img2 from "../assets/img/Blackjack/proj1Img2.png";
+import proj1Img3 from "../assets/img/Blackjack/proj1Img3.png";
+import proj1Img4 from "../assets/img/Blackjack/proj1Img4.png";
+import proj1Img5 from "../assets/img/Blackjack/proj1Img5.png";
+import proj2Img1 from "../assets/img/Anidex/proj2Img1.png";
+import proj2Img2 from "../assets/img/Anidex/proj2Img2.png";
+import proj2Img3 from "../assets/img/Anidex/proj2Img3.png";
+import proj2Img4 from "../assets/img/Anidex/proj2Img4.png";
+import proj2Img5 from "../assets/img/Anidex/proj2Img5.png";
+import proj2Img6 from "../assets/img/Anidex/proj2Img6.png";
+import proj3Img1 from "../assets/img/Need/proj3Img1.png";
+import proj3Img2 from "../assets/img/Need/proj3Img2.png";
+import proj3Img3 from "../assets/img/Need/proj3Img3.png";
+import proj3Img4 from "../assets/img/Need/proj3Img4.png";
+import proj3Img5 from "../assets/img/Need/proj3Img5.png";
+import proj3Img6 from "../assets/img/Need/proj3Img6.png";
+import proj4Img1 from "../assets/img/Fittrax/proj4Img1.png";
+import proj4Img2 from "../assets/img/Fittrax/proj4Img2.png";
+import proj4Img3 from "../assets/img/Fittrax/proj4Img3.png";
+import proj4Img4 from "../assets/img/Fittrax/proj4Img4.png";
+import proj4Img5 from "../assets/img/Fittrax/proj4Img5.png";
+import proj4Img6 from "../assets/img/Fittrax/proj4Img6.png";
+
+
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -16,12 +38,24 @@ export const Projects = () => {
       codeUrl: "https://github.com/carlosm22700/blackjack-project-one",
       details: [
         {
-          description: "Screenshot 1 description",
-          imgUrl: projImg1,
+          description: "Get Started",
+          imgUrl: proj1Img1,
         },
         {
-          description: "Screenshot 2 description",
-          imgUrl: projImg2,
+          description: "Place A Bet",
+          imgUrl: proj1Img3,
+        },
+        {
+          description: "First Hand",
+          imgUrl: proj1Img2,
+        },
+        {
+          description: "Hand Resolved",
+          imgUrl: proj1Img4,
+        },
+        {
+          description: "Game Over!",
+          imgUrl: proj1Img5,
         },
       ],
     },
@@ -31,12 +65,28 @@ export const Projects = () => {
       codeUrl: "https://github.com/carlosm22700/Anime-Progress-Tracker",
       details: [
         {
-          description: "Screenshot 1 description",
-          imgUrl: projImg3,
+          description: "Homepage",
+          imgUrl: proj2Img1,
         },
         {
-          description: "Screenshot 2 description",
-          imgUrl: projImg1,
+          description: "Add an Anime",
+          imgUrl: proj2Img2,
+        },
+        {
+          description: "View Anime",
+          imgUrl: proj2Img3,
+        },
+        {
+          description: "Update Anime",
+          imgUrl: proj2Img4,
+        },
+        {
+          description: "Delete Anime",
+          imgUrl: proj2Img5,
+        },
+        {
+          description: "View Details",
+          imgUrl: proj2Img6,
         },
       ],
     },
@@ -46,12 +96,28 @@ export const Projects = () => {
       codeUrl: "https://github.com/carlosm22700/NEED-ecommerce",
       details: [
         {
-          description: 'Screenshot 1 des',
-          imgUrl: projImg1,
+          description: 'Auth Page',
+          imgUrl: proj3Img1,
         },
         {
-          description: 'Screenshot 2 des',
-          imgUrl: projImg2
+          description: 'Shop Page',
+          imgUrl: proj3Img2,
+        },
+        {
+          description: 'Details Page',
+          imgUrl: proj3Img3,
+        },
+        {
+          description: 'Cart Page',
+          imgUrl: proj3Img4
+        },
+        {
+          description: 'Checkout Page',
+          imgUrl: proj3Img5,
+        },
+        {
+          description: 'Confirmation Page',
+          imgUrl: proj3Img6,
         },
       ]
     },
@@ -61,12 +127,28 @@ export const Projects = () => {
       codeUrl: "https://github.com/carlosm22700/Fitness-Tracker",
       details: [
         {
-          description: 'Screenshot 1 des',
-          imgUrl: projImg1,
+          description: 'Auth Page',
+          imgUrl: proj4Img6,
         },
         {
-          description: 'Screenshot 2 des',
-          imgUrl: projImg2
+          description: 'Home Page',
+          imgUrl: proj4Img1
+        },
+        {
+          description: 'Add Routine',
+          imgUrl: proj4Img2
+        },
+        {
+          description: 'View Routines',
+          imgUrl: proj4Img3
+        },
+        {
+          description: 'Search Exercise',
+          imgUrl: proj4Img4
+        },
+        {
+          description: 'Add Exercise',
+          imgUrl: proj4Img5
         },
       ]
     }
